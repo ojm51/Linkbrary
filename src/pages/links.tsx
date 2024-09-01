57b5b69b7a10ec2ce2baf9d1e59fd679b95209df
Jeongmin Oh <ojm51@naver.com>
2024-09-01 14:53:08 +0900

    Feat: 링크페이지 링크추가바 구현 - #10

---
+++ src/pages/links.tsx
@@ -0,0 +1,11 @@
+import AddLink from '../LinksComponents/components/AddLink';
+
+function links() {
+  return (
+    <div className="h-[220px] pt-[60px] bg-bg">
+      <AddLink />
+    </div>
+  );
+}
+
+export default links;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/styles/**/*.css',
+    './src/LinksComponents/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
