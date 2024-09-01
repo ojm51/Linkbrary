0a8bece86537ee7e5ee749ee9207a48eba1459ef
L1m3Kun <sungku2757@gmail.com>
2024-09-01 14:48:16 +0900

    fix: 회원가입 버튼 컴포넌트 수정 - #2
    disabled ui 추가
    
    일반 버튼 추가
    
    className 대체 형식으로 수정

--- src/components/common/buttons/CommonButton.tsx
+++ src/components/common/buttons/CommonButton.tsx
@@ -20,14 +20,23 @@ export const CommonButton = ({
   ...rest
 }: CommonButtonProps) => {
   const defaultButtonClassName =
-    'flex w-full py-4 px-5 justify-center items-center gap-2.5 bg-gradient-color rounded-lg text-white';
+    'flex w-full py-4 px-5 justify-center items-center gap-2.5 bg-gradient-color rounded-lg text-white disabled:bg-none disabled:bg-gray-500';
   const TsButton: TsButtonMode = { type: mode };
   const CButton = match(TsButton)
-    .with({ type: 'default' }, () => <></>)
+    .with({ type: 'default' }, () => (
+      <button
+        type="button"
+        className={className ?? defaultButtonClassName}
+        onClick={onClick}
+        {...rest}
+      >
+        {children}
+      </button>
+    ))
     .with({ type: 'submit' }, () => (
       <button
         type="submit"
-        className={`${defaultButtonClassName} ${className}`}
+        className={className ?? defaultButtonClassName}
         onClick={onClick}
         {...rest}
       >
