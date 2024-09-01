57500e14322be414eb83c91e9273b8d96de23017
Jeongmin Oh <ojm51@naver.com>
2024-09-02 00:02:26 +0900

    Chore: 링크페이지 컴포넌트 파일 위치 변경 - #10

--- src/pages/links/index.tsx
+++ src/pages/links/index.tsx
@@ -1,7 +1,7 @@
-import AddLink from '@/LinksComponents/AddLink';
-import SearchBar from '@/LinksComponents/SearchBar';
-import FolderList from '@/LinksComponents/FolderList/FolderList';
-import FolderMenuList from '@/LinksComponents/FolderMenuList/FolderMenuList';
+import AddLink from '@/components/links/AddLink';
+import SearchBar from '@/components/links/SearchBar';
+import FolderList from '@/components/links/FolderList/FolderList';
+import FolderMenuList from '@/components/links/FolderMenuList/FolderMenuList';
 
 function links() {
   return (
@@ -9,11 +9,15 @@ function links() {
       <div className="h-[220px] pt-[60px] bg-bg">
         <AddLink />
       </div>
-      <SearchBar />
-      <FolderList />
-      <div className="flex justify-between items-center">
-        <h3 className="font-semibold text-2xl text-black">title</h3>
-        <FolderMenuList />
+      <div className="max-w-[1060px] m-auto">
+        <div className="my-10">
+          <SearchBar />
+        </div>
+        <FolderList />
+        <div className="flex justify-between items-center">
+          <h3 className="font-semibold text-2xl text-black my-6">title</h3>
+          <FolderMenuList />
+        </div>
       </div>
     </>
   );
