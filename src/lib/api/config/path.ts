cf268f9ab08f6af37e3f799e74e1fc890db06d1b
L1m3Kun <sungku2757@gmail.com>
2024-09-02 15:16:37 +0900

    feat: 공통 API path 설정 추가 - #21

---
+++ src/lib/api/config/path.ts
@@ -0,0 +1,37 @@
+const API_PATH = {
+  auth: {
+    signUp: '/auth/sign-up',
+    Login: '/auth/sign-in',
+    googleSignUp: '/auth/sign-up/google',
+    kakaoSignUp: '/auth/sign-up/kakao',
+    googleLogin: '/auth/sign-in/google',
+    kakaoSLogin: '/auth/sign-in/kakao',
+  },
+  folder: {
+    default: '/folders',
+    detail(id: string | number) {
+      return `/folders/${id}`;
+    },
+  },
+  link: {
+    default: '/links',
+    detail(id: string | number) {
+      return `/links/${id}`;
+    },
+    category(id: string | number) {
+      return `/folders/${id}/links`;
+    },
+  },
+  favorite: {
+    default: '/favorites',
+  },
+  oauth: {
+    default: '/oauthApps',
+  },
+  user: {
+    default: '/users',
+    emailCheck: '/users/check-email',
+  },
+};
+
+export default API_PATH;
