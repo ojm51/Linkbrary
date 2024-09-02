aa3f85c4d999b36c0eac38705dd18abe377f3d07
L1m3Kun <sungku2757@gmail.com>
2024-09-03 02:10:02 +0900

    feat: 로그인 유저 정보 context handler 추가 - #5

---
+++ src/lib/context/useAuthHandler.ts
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+import { LoginParams, UserInfoDTO } from '../api';
+import { getFromStorage, setToStorage } from '../storage';
+import { useGetUserInfo, useLogin } from '../hooks';
+
+interface UserInfo extends UserInfoDTO {
+  accessToken: string;
+}
+
+export interface AuthContextType {
+  userInfo: UserInfo | null;
+  isLoggedin: boolean;
+  login: (params: LoginParams) => void;
+  logout: () => void;
+  updateUserInfo: (items: UserInfo) => void;
+}
+
+export const useAuthHandler = () => {
+  const router = useRouter();
+  const loginMutate = useLogin();
+
+  const [accessToken, setAccessToken] = useState<string>(
+    getFromStorage('userInfo')?.accessToken ?? '',
+  );
+  const { data } = useGetUserInfo({
+    accessToken,
+  });
+
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+  const [isLoggedin, setIsLoggedin] = useState<boolean>(false);
+
+  const login = async ({ email, password }: LoginParams) => {
+    await loginMutate.mutateAsync(
+      { email, password },
+      {
+        onSuccess(response) {
+          const { accessToken } = response.data;
+          if (accessToken) {
+            setAccessToken(accessToken);
+          }
+          setIsLoggedin(true);
+        },
+        onError() {
+          /** @Todo 에러 메세지 모달 처리 */
+        },
+      },
+    );
+  };
+
+  const logout = () => {
+    setUserInfo(null);
+    setIsLoggedin(false);
+    localStorage.removeItem('userInfo');
+    router.push('/');
+  };
+
+  const updateUserInfo = (newInfo: UserInfo) => {
+    setUserInfo(newInfo);
+    setToStorage('userInfo', newInfo);
+  };
+
+  const authProviderValue = {
+    userInfo: userInfo,
+    isLoggedin,
+    login,
+    logout,
+    updateUserInfo,
+  };
+
+  useEffect(() => {
+    if (data) {
+      const newUserInfo = data.data;
+      updateUserInfo({ ...newUserInfo, accessToken });
+    }
+  }, [data]);
+
+  return { authProviderValue };
+};
