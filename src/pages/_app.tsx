4b6bba57cb3a0b1b72a9c51a8922d44a7cb16157
yamdang <youm4000@naver.com>
2024-09-02 17:42:47 +0900

    style: 랜딩페이지 애니메이션 css 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css';
+import '@/styles/landingEffect.css';
 import Header from '@/components/layout/Header';
 import type { AppProps } from 'next/app';
 import Footer from '@/components/layout/Footer';
