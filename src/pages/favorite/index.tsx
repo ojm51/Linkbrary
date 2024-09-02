26f4deab54a63bc381b2ae1ba43636b6c9a480eb
DasomKim <ekt0521@naver.com>
2024-09-02 15:18:55 +0900

    feat: 즐겨찾기 ui 추가

---
+++ src/pages/favorite/index.tsx
@@ -0,0 +1,11 @@
+const Favorite = () => {
+  return (
+    <div className="bg-[#F0F6FF]">
+      <h1 className="font-pretendard text-[40px] font-semibold leading-[47.73px] text-center">
+        ⭐️즐겨찾기
+      </h1>
+    </div>
+  );
+};
+
+export default Favorite;
