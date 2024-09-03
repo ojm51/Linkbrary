98c81c53fa89c43c6e0f25f051f830decfc6d454
yamdang <youm4000@naver.com>
2024-09-03 17:04:49 +0900

    refactor: 랜딩페이지 푸터 sns리스트 객체로 관리

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,7 +5,41 @@ import YoutubeIcon from '@/assets/icons/youtubeIcon.svg';
 import InstagramIcon from '@/assets/icons/instagramIcon.svg';
 import Image from 'next/image';
 
+type SnsListType = {
+  id: number;
+  href: string;
+  src: string;
+  alt: string;
+};
+
 const Footer = () => {
+  const SNS_LISTS: SnsListType[] = [
+    {
+      id: 1,
+      href: '/',
+      src: FacebookIcon,
+      alt: '페이스북 아이콘',
+    },
+    {
+      id: 2,
+      href: '/',
+      src: TwitterIcon,
+      alt: '트위터 아이콘',
+    },
+    {
+      id: 3,
+      href: '/',
+      src: YoutubeIcon,
+      alt: '유튜브 아이콘',
+    },
+    {
+      id: 4,
+      href: '/',
+      src: InstagramIcon,
+      alt: '인스타그램 아이콘',
+    },
+  ];
+
   return (
     <footer className="w-full h-[160px] pt-[32px] bg-black">
       <div className="flex justify-between items-center w-full max-w-[1750px] px-[20px] mx-auto">
@@ -19,46 +53,15 @@ const Footer = () => {
           </Link>
         </div>
         <ul className="flex justify-center items-center gap-[12px] w-full max-w-[120px]">
-          <li>
-            <Link href="/">
-              <Image
-                width={20}
-                height={20}
-                src={FacebookIcon}
-                alt="페이스북 아이콘"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image
-                width={20}
-                height={20}
-                src={TwitterIcon}
-                alt="트위터 아이콘"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image
-                width={20}
-                height={20}
-                src={YoutubeIcon}
-                alt="유튜브 아이콘"
-              />
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image
-                width={20}
-                height={20}
-                src={InstagramIcon}
-                alt="인스타그램 아이콘"
-              />
-            </Link>
-          </li>
+          {SNS_LISTS.map((sns) => {
+            return (
+              <li key={sns.id}>
+                <Link href={sns.href}>
+                  <Image width={20} height={20} src={sns.src} alt={sns.alt} />
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </footer>
