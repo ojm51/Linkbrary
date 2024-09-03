aade21cda8b5833733e32e1ec6f2e7b9247b7bed
L1m3Kun <sungku2757@gmail.com>
2024-09-03 14:31:26 +0900

    feat: 로그인 로그인 api 호출 추가 - #5

--- src/lib/hooks/query/auth/useLogin.ts
+++ src/lib/hooks/query/auth/useLogin.ts
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
 import { useMutation } from '@tanstack/react-query';
+
 import { login } from '@/lib/api';
+import { AuthContext } from '@/lib/context';
+
 import { MUTATION_KEY } from '../config';
 
 export const useLogin = () => {
+  const { logout } = useContext(AuthContext);
   return useMutation({
     mutationKey: [MUTATION_KEY.login],
     mutationFn: login,
+    onError() {
+      logout();
+    },
   });
 };
