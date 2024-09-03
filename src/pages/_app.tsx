228a59d41970d471e430b56c4fad7f7a4d602c93
yamdang <youm4000@naver.com>
2024-09-03 17:06:14 +0900

    feat: 공통 로그인, 회원가입 페이지에서는 네비바와 푸터 미노출

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
 import '@/styles/globals.css';
 import '@/styles/landingEffect.css';
-import Header from '@/components/layout/Header';
 import type { AppProps } from 'next/app';
+import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
+import { Routes } from '@/lib/route/index';
+import { useRouter } from 'next/router';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  const pathname = router.pathname as Routes;
+  const isAuthPage = [Routes.LOGIN, Routes.SIGNUP].includes(pathname);
+
   return (
     <>
-      <Header />
+      {!isAuthPage && <Header />}
       <div className="custom-footer-height">
         <Component {...pageProps} />
       </div>
-      <Footer />
+      {!isAuthPage && <Footer />}
     </>
   );
 };
