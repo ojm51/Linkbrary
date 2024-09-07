2d9bb59edbafa5c30731508d2f16cf5391b860a6
Jeongmin Oh <ojm51@naver.com>
2024-09-07 15:12:53 +0900

    Feat: 링크페이지 링크 추가 및 폴더 이름 변경 기능 구현 - #31

--- src/components/modals/ModalContents/ChangeFolderName.tsx
+++ src/components/modals/ModalContents/ChangeFolderName.tsx
@@ -1,9 +1,16 @@
 import { useContext } from 'react';
 import { FolderContext } from '@/lib/context';
 import { CommonInput, CommonButton } from '@/components';
+import { putFolder } from '@/lib/api';
 
 export const ChangeFolderName = () => {
   const { selectedFolder } = useContext(FolderContext);
+
+  const handleChangeButtonClick = () => {
+    const folderName = selectedFolder.name;
+    const folderId = selectedFolder.id;
+    putFolder({ folderName, folderId });
+  };
 
   return (
     <div>
@@ -11,12 +18,14 @@ export const ChangeFolderName = () => {
         폴더 이름 변경
       </h3>
       <h4 className="mt-2 mb-6 text-center text-[14px] font-normal text-secondary-60 font-[Pretendard] not-italic leading-[22px]">
-        {selectedFolder}
+        {selectedFolder.name}
       </h4>
       <div className="mt-6 mb-[15px]">
         <CommonInput placeholder="내용 입력" />
       </div>
-      <CommonButton mode="default">변경하기 </CommonButton>
+      <CommonButton mode="default" onClick={handleChangeButtonClick}>
+        변경하기{' '}
+      </CommonButton>
     </div>
   );
 };
