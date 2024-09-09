f8c730b20269cb2bdd3349c71bfd371e36a8fb2c
L1m3Kun <sungku2757@gmail.com>
2024-09-09 17:55:06 +0900

    feat: 로그인 SNS - Kakao 로그인 api 추가 - #7

--- src/lib/api/auth/apis.ts
+++ src/lib/api/auth/apis.ts
@@ -4,6 +4,9 @@ import {
   EmailConfirmParams,
   LoginParams,
   SignUpParams,
+  SocialLoginDTO,
+  SocialLoginParams,
+  SocialSignUpParams,
 } from './types';
 
 export const signUp = async ({ email, name, password }: SignUpParams) => {
@@ -30,6 +33,51 @@ export const login = async ({ email, password }: LoginParams) => {
   const params = { email, password };
   const response = await instance
     .post<AuthDTO>(API_PATH.auth.Login, JSON.stringify(params))
+    .then((res) => res)
+    .catch(() => {});
+
+  return response;
+};
+
+export const socialSignUp = async ({
+  socialProvider,
+  name,
+  token,
+}: SocialSignUpParams) => {
+  const params = {
+    name,
+    token,
+    redirectUri: process.env.NEXT_PUBLIC_OAUTH_REDIRECT_URI,
+  };
+
+  const url =
+    socialProvider === 'google'
+      ? API_PATH.auth.googleSignUp
+      : API_PATH.auth.kakaoSignUp;
+
+  const response = await instance
+    .post(url, JSON.stringify(params))
+    .then((res) => res)
+    .catch(() => {});
+
+  return response;
+};
+
+export const socialLogin = async ({
+  socialProvider,
+  token,
+}: SocialLoginParams) => {
+  const params = {
+    token,
+    redirectUri: process.env.NEXT_PUBLIC_OAUTH_LOGIN_REDIRECT_URI,
+  };
+  const url =
+    socialProvider === 'google'
+      ? API_PATH.auth.googleLogin
+      : API_PATH.auth.kakaoLogin;
+
+  const response = await instance
+    .post<SocialLoginDTO>(url, JSON.stringify(params))
     .then((res) => res)
     .catch(() => {});
 
