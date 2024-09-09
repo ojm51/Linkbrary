d69ef21e070008aa4a78a47185fdc1930d85f1e1
L1m3Kun <sungku2757@gmail.com>
2024-09-09 17:51:31 +0900

    style: 로그인/회원가입 배경화면 너비 변경 - #2 #4

--- src/components/auth/Auth.style.ts
+++ src/components/auth/Auth.style.ts
@@ -1,4 +1,4 @@
-const BackGroundStyle = 'w-full h-full bg-bg py-[7.5rem]';
+const BackGroundStyle = 'w-full h-screen overflow-scroll bg-bg py-[7.5rem]';
 
 const WrapperStyle =
   'flex flex-col max-w-[25rem] justify-center items-center gap-[2rem] m-auto';
