777cebcb7f40d7f20b78220a7222dd36d566c631
L1m3Kun <sungku2757@gmail.com>
2024-09-11 03:30:03 +0900

    feat: 공통 모달 context 추가 - #1 #49

--- src/lib/context/ModalProvider.tsx
+++ src/lib/context/ModalProvider.tsx
@@ -1,5 +1,6 @@
-import { createContext } from 'react';
+import { createContext, useContext } from 'react';
 import { ModalContextType, useModalHandler } from './useModalHandler';
+import { Modal } from '@/components/common/modal/ModalContainer';
 
 export const ModalContext = createContext<ModalContextType>({
   modalList: [],
@@ -12,10 +13,22 @@ interface ModalProviderProps {
 }
 
 export const ModalProvider = ({ children }: ModalProviderProps) => {
-  const modalProviderValue = useModalHandler();
+  const { modalList, openModal, closeModal } = useModalHandler();
+
   return (
-    <ModalContext.Provider value={modalProviderValue}>
+    <ModalContext.Provider value={{ modalList, openModal, closeModal }}>
       {children}
+      {modalList.map(({ key, ...rest }) => {
+        return <Modal key={key} modalName={key} {...rest} />;
+      })}
     </ModalContext.Provider>
   );
+};
+
+export const useModal = () => {
+  const modalCtx = useContext(ModalContext);
+  if (!modalCtx) {
+    throw new Error('컨텍스트는 프로바이더 안에서만 사용가능합니다.');
+  }
+  return modalCtx;
 };
