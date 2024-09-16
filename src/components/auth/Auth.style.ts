19d81763522c704a006abfca55790144dd5da95e
L1m3Kun <sungku2757@gmail.com>
2024-09-17 01:29:04 +0900

    feat: 로그인/회원가입 반응형 ui 추가 - #2 #4

--- src/components/auth/Auth.style.ts
+++ src/components/auth/Auth.style.ts
@@ -1,4 +1,5 @@
-const BackGroundStyle = 'w-full h-screen overflow-scroll bg-bg py-[7.5rem]';
+const BackGroundStyle =
+  'w-full h-screen overflow-scroll bg-bg py-[7.5rem] md:py-[12.5rem] sm:py-[7.5rem] sm:px-8';
 
 const WrapperStyle =
   'flex flex-col max-w-[25rem] justify-center items-center gap-[2rem] m-auto';
