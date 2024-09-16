7e4978e05237a1121548f3099794e0c1ca6526dc
Jeongmin Oh <ojm51@naver.com>
2024-09-16 11:37:37 +0900

    Feat: 링크페이지 오류 모달 추가 - #52

--- src/lib/context/useFolderHandler.ts
+++ src/lib/context/useFolderHandler.ts
@@ -1,20 +1,32 @@
 import { useCallback, useEffect, useState } from 'react';
 import { FolderTypes, getFolderList } from '@/lib/api';
+import { useModal } from '@/lib/context';
 import { useAuth } from './AuthProvider';
 
 export const useFolderHandler = () => {
+  const { openModal } = useModal();
+
+  const getInitialFolder = () => {
+    return { createdAt: '', id: 0, name: '전체' };
+  };
+
   const { isLoggedin } = useAuth();
   const [folderList, setFolderList] = useState<FolderTypes[]>([]);
-  const [selectedFolder, setSelectedFolder] = useState<FolderTypes>({
-    createdAt: '',
-    id: 0,
-    name: '전체',
-  });
+  const [selectedFolder, setSelectedFolder] =
+    useState<FolderTypes>(getInitialFolder);
 
   const fetchFolderList = useCallback(async () => {
-    const data = await getFolderList();
-    setFolderList(data);
-  }, [setFolderList]);
+    try {
+      const data = await getFolderList();
+      setFolderList(data);
+    } catch {
+      openModal({
+        type: 'alert',
+        key: 'getFolderError400',
+        message: `폴더를 가져오는 중 오류가 발생했습니다. 다시 시도해주세요.`,
+      });
+    }
+  }, []);
 
   useEffect(() => {
     if (isLoggedin) {
